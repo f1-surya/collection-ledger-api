52022713ad86814be410d48ebe571a9028b441b4
surya <balasurya811@gmail.com>
2025-08-19 18:45:33 +0530

    Migrated from MySQL to PostgreSQL.

--- src/main/java/com/surya/customerledger/db/model/User.java
+++ src/main/java/com/surya/customerledger/db/model/User.java
@@ -11,7 +11,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import java.util.Collection;
 import java.util.List;
 
-@Entity
+@Entity(name = "users")
 public class User implements UserDetails {
 
   @Id
